d236b788b36471b76682fab12b1cbaf13b02e0e0
glennkok1501 <kok.glenn@yahoo.com.sg>
2022-11-30 20:14:21 +0800

    removed react router

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import Topbar from './Components/Navigation/Topbar';
 import Index from './Pages/Index';
 
 function App() {
   return (
-      <BrowserRouter>
-        <div className='app'>
-          <Topbar />
-            <Switch>
-              <Route exact path="/">
-                <Index />
-              </Route>
-            </Switch>
-        </div>
-
-        
-        
-      </BrowserRouter>
-    
+      <div className='app'>
+        <Topbar />
+        <Index />
+      </div>
   );
 }
 
