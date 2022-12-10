d653d3d3bd956b6ef374e57baf63809f8de9d684
glennkok1501 <kok.glenn@yahoo.com.sg>
2022-12-10 13:23:50 +0800

    skills updated

--- src/Components/About/Skills.js
+++ src/Components/About/Skills.js
@@ -9,12 +9,12 @@ const Skills = () => {
     return ( 
         <>
             <Skillsbar title="Malware Analysis" level="80" />
+            <Skillsbar title="Ethical Hacking" level="82" />
             <Skillsbar title="Enterprise Networking" level="90" />
             <Skillsbar title="Digital Forensics (EnCase)" level="70" />
             <Skillsbar title="Computer Operating Systems (Windows & Linux)" level="90" />
             <Skillsbar title="Programming (Python3, C#, JavaScript, Java)" level="95" />
-            <Skillsbar title="Secure Software Development" level="60" />
-            <Skillsbar title="Fullstack Development" level="90" />
+            <Skillsbar title="Secure Software / Fullstack Development" level="90" />
 
         </>
      );
