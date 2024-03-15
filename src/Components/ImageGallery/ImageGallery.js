586c1120853b327578545300b4f0ab6ad76b5924
YeppeunYena <yeppeunyena1999@gmail.com>
2024-03-15 10:58:04 +0800

    implemented image sliders

--- src/Components/ImageGallery/ImageGallery.js
+++ src/Components/ImageGallery/ImageGallery.js
@@ -1,12 +1,23 @@
 
-import { mdiClose } from "@mdi/js";
+import { mdiChevronLeft, mdiChevronRight, mdiClose } from "@mdi/js";
 import Icon from "@mdi/react";
 import { useState } from "react";
 import { Modal } from "react-bootstrap";
 import ImagePreview from "./ImagePreview";
 
 const ImageGallery = ({images}) => {
-    const [selected, setSelected] = useState(null)
+    const [show, setShow] = useState(false)
+    const [selected, setSelected] = useState(0)
+
+    const next = () => {
+        setSelected((selected + 1) % images.length)
+        console.log(selected)
+    }
+
+    const previous = () => {
+        setSelected((selected - 1) % images.length)
+        console.log(selected)
+    }
 
     return ( 
         <>
@@ -15,23 +26,35 @@ const ImageGallery = ({images}) => {
                 Images
             </h3>
             <div className="d-flex"  style={{"overflowY": "auto"}}>
+
                 {
-                    images.map(image => (
+                    images.map((image, i) => (
                         <div key={image}>
-                            <ImagePreview image={image} setSelected={setSelected} />
+                            <ImagePreview image={images[i]} setSelected={setSelected} index={i} setShow={setShow} />
+
                         </div>
                     ))
                 }
             </div>
 
-            <Modal className="bg-dark" backdrop="static" animation={false} centered size="lg" show={selected}>
-                    <div className="position-relative">
-                        <div className="btn position-absolute top-0 end-0" onClick={() => setSelected(null)}>
+            <Modal className="bg-dark" backdrop="static" animation={false} centered size="lg" show={show}>
+                    <div className="position-relative align-items-center d-flex">
+                        <img src={images[selected]} alt='project_img' className="w-100 rounded img-fluid" />
+
+                        <div className="btn position-absolute top-0 end-0" onClick={() => setShow(false)}>
                             <Icon size={1.5} path={mdiClose} className="text-dark"/>
+                        </div>
+                        <div className="d-flex w-100 justify-content-between position-absolute">
+                            <i className="btn p-0" onClick={() => previous()}>
+                                <Icon size={2.5} path={mdiChevronLeft} className="text-dark"/>
+                            </i>
+                            <i className="btn p-0" onClick={() => next()}>
+                                <Icon size={2.5} path={mdiChevronRight} className="text-dark"/>
+                            </i>
+
                         </div>
 
                     </div>
-                    <img src={selected} alt='project_img' className="img-fluid rounded" />
 
             </Modal>
         </>
