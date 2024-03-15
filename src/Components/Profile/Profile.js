2f5f2cc1d3babfc6300aabb4f90fcca9a768940f
YeppeunYena <yeppeunyena1999@gmail.com>
2024-03-15 10:07:05 +0800

    changed avatar

--- src/Components/Profile/Profile.js
+++ src/Components/Profile/Profile.js
@@ -1,5 +1,5 @@
-// import profile_img from '../../Assets/fomal-pic_orig.png'
-import profile_img from '../../Assets/avatar.jpg'
+import profile_img from '../../Assets/fomal-pic_orig.png'
+// import profile_img from '../../Assets/avatar.jpg'
 
 import Socials from './Socials';
 
