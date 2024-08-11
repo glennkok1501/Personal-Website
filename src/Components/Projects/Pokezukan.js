97b5e7d733fe24ba8b9f6be6c5e10ffec27565c3
glennkok1501 <kok.glenn@yahoo.com.sg>
2024-08-11 18:00:42 +0800

    updated project 11

--- src/Components/Projects/Pokezukan.js
+++ src/Components/Projects/Pokezukan.js
@@ -1,7 +1,27 @@
+import { mdiDownload, mdiGithub, mdiStore } from '@mdi/js';
 import logo from '../../Assets/projects/pokezukan/pokezukan.png'
 import ImageGallery from '../ImageGallery/ImageGallery';
+import Icon from '@mdi/react';
 
 const Pokezukan = () => {
+
+    const moreDetails = [
+        {
+            "name": "Amazon Appstore",
+            "link": "https://www.amazon.com/gp/product/B09GJGY1L3",
+            "icon": mdiStore
+        },
+        {
+            "name": "GitHub",
+            "link": "https://github.com/glennkok1501/PokeZukan-MobileApp",
+            "icon": mdiGithub
+        },
+        {
+            "name": "Download",
+            "link": "https://github.com/glennkok1501/PokeZukan-MobileApp/raw/main/app/release/app-release.apk",
+            "icon": mdiDownload
+        },
+    ]
     return ( 
         <>
             <div className="row align-items-center">
@@ -12,14 +32,19 @@ const Pokezukan = () => {
                     <h5>
                     Simple and easy to use encyclopedia for Pokémon.
                     </h5>
-                    <p>
+                    <p className='d-none d-md-block'>
                     PokeZukan is an unofficial app that offers a free and minimalistic Pokédex.
                     It is especially catered for beginners starting the Pokémon game with the basic information needed to progress their journey.
                     Network connection is needed as data is retrieved from <a href="https://github.com/glennkok1501/PokeZukanAPI" rel="noopener noreferrer" target='_blank'>PokeZukanAPI</a>.
                     </p>
                 </div>
             </div>
-            <h5 className="mt-3">DISCLAIMER</h5>
+            <p className='mt-2 d-lg-none'>
+            PokeZukan is an unofficial app that offers a free and minimalistic Pokédex.
+            It is especially catered for beginners starting the Pokémon game with the basic information needed to progress their journey.
+            Network connection is needed as data is retrieved from <a href="https://github.com/glennkok1501/PokeZukanAPI" rel="noopener noreferrer" target='_blank'>PokeZukanAPI</a>.
+            </p>
+            <h5 className="mt-2">DISCLAIMER</h5>
             <p>PokeZukan is an unofficial, free fan made app and is not affiliated, endorsed or supported by Nintendo, GAME FREAK or The Pokémon Company in any way. Some images used in this app are copyrighted and are supported under fair use. Arts, visuals, names, Pokémon and Pokémon names are properties of the companies mentioned previously.
             No copyright or trademark infringement is intended.
             © 2021 Pokémon.
@@ -31,6 +56,18 @@ const Pokezukan = () => {
                 <li>Learn about the different Abilities each Pokémon has</li>
                 <li>Study a variety of moves used in the Pokémon world</li>
             </ul>
+
+            <b>Find out more below:</b>
+            <div className='d-flex justify-content-around'>
+                {
+                    moreDetails.map((i) => (
+                        <a key={i.name} className='btn text-white' target="_blank" rel="noreferrer" href={i.link}>
+                            <Icon className='text-white' path={i.icon} size={1} /> {i.name}
+                        </a>
+                    ))
+                }
+                
+            </div>
 
             <ImageGallery images={[
                 "https://raw.githubusercontent.com/glennkok1501/PokeZukan-MobileApp/main/images/1.jpg",
--- src/Components/Projects/Spendid.js
+++ src/Components/Projects/Spendid.js
@@ -71,23 +71,6 @@ const Spendid = () => {
                 
             </div>
             <ImageGallery images={[img_1, img_2, img_3, img_4, img_5, img_6, img_7, img_8, img_9, img_10]} />
-
-            <div className="row">
-                <div className="col-md-6">
-                </div>
-                <div className="col-md-6">
-                    
-                    
-                </div>
-            </div>
-            <div className='row'>
-                <div className='col-md-6'>
-                </div>
-                <div className='col-md-6'>
-                     
-                    
-                </div>
-            </div>
         </> 
         
      );
